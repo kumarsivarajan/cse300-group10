345ec97f31c23198d3ac114be4bbb0a90a8f7bdd
manik10044@iiitd.ac.in <manik10044@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-09-08 08:45:38 +0000

    Made a form function and used it in the registration

--- trunk/application/helpers/MY_form_helper.php
+++ trunk/application/helpers/MY_form_helper.php
@@ -9,3 +9,96 @@ function form_input_formatted($data = '', $value = '', $extra = '')
     		$prefix=$prefix.$data['label'];
 		return $prefix."<input "._parse_form_attributes($data, $defaults).$extra." />".$suffix;
 }
+function form_input_infield($data = '', $value = '', $extra = '')
+{
+    $defaults = array('type' => 'text', 'name' => (( ! is_array($data)) ? $data : ''), 'value' => $value);
+    	$prefix="<p>";
+    	$suffix="</p>";
+    	if(isset($data['label'])&&isset($data['id']))
+    		$prefix=$prefix."<label for=".$data['id'].">".$data['label']."</label>";
+		return $prefix."<input "._parse_form_attributes($data, $defaults).$extra." />".$suffix;
+}
+function generate_form($action='',$elements='',$attr=''){
+	$html='';
+	if(!isset($action))
+		return '';
+	if(is_array($attr))
+		$html.=form_open($action,$attr);
+	else
+		$html.=form_open($action);
+	if(is_array($elements)){
+		foreach($elements as $elem){
+			if($elem['input']=="text"){
+				unset($elem['input']);
+				$html.=form_input_infield($elem);
+				}
+			else if($elem['input']=="select"){
+				$html.=form_dropdown_formatted($elem['name'], $elem['values']);
+			}
+			else if($elem['input']=="submit"){
+				$html.=form_submit('', $elem['value']);
+			}
+			
+		
+	}
+	
+	}
+	$html.=form_close();
+	return $html;
+}
+	function form_dropdown_formatted($name = '', $options = array(), $selected = array(), $extra = '')
+	{	$label='';
+		if(is_array($name))
+		{	$label=$name['label'];
+			$name=$name['name'];
+		}
+		if ( ! is_array($selected))
+		{
+			$selected = array($selected);
+		}
+
+		// If no selected state was submitted we will attempt to set it automatically
+		if (count($selected) === 0)
+		{
+			// If the form name appears in the $_POST array we have a winner!
+			if (isset($_POST[$name]))
+			{
+				$selected = array($_POST[$name]);
+			}
+		}
+
+		if ($extra != '') $extra = ' '.$extra;
+
+		$multiple = (count($selected) > 1 && strpos($extra, 'multiple') === FALSE) ? ' multiple="multiple"' : '';
+		
+		$form ='<p>'.$label. '<select name="'.$name.'"'.$extra.$multiple.">\n";
+
+		foreach ($options as $key => $val)
+		{
+			$key = (string) $key;
+
+			if (is_array($val) && ! empty($val))
+			{
+				$form .= '<optgroup label="'.$key.'">'."\n";
+
+				foreach ($val as $optgroup_key => $optgroup_val)
+				{
+					$sel = (in_array($optgroup_key, $selected)) ? ' selected="selected"' : '';
+
+					$form .= '<option value="'.$optgroup_key.'"'.$sel.'>'.(string) $optgroup_val."</option>\n";
+				}
+
+				$form .= '</optgroup>'."\n";
+			}
+			else
+			{
+				$sel = (in_array($key, $selected)) ? ' selected="selected"' : '';
+
+				$form .= '<option value="'.$key.'"'.$sel.'>'.(string) $val."</option>\n";
+			}
+		}
+		
+		$form .= '</select></p>';
+
+		return $form;
+	}
--- trunk/application/views/admin_login.php
+++ trunk/application/views/admin_login.php
@@ -3,19 +3,23 @@
 <head>
 	<meta charset="utf-8">
 	<title>Admin - Hostel Allocation System - <?php echo $ins_name ?></title>
-	<?php foreach($css as $cssfile):?>
-	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile) ?>"/>
+		<?php foreach($css as $cssfile):?>
+	    	<link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile) ?>"/>
 	    <?php endforeach;?>
+	    <?php foreach($scripts as $script):?>
+	    	<script type="text/javascript" src="<?php echo base_url("/application/js/".$script) ?>"></script>
+	    <?php endforeach;?>
+	 <script type="text/javascript">
+	 $(document).ready(function(){
+	 $("label").inFieldLabels();
+	 });
+	 </script>
 </head>
 <body>
 
 <div class="login_bar">
 		<div id="body">
-			<?php echo form_open('Admin/validate','');?>
-			<?php
-				foreach($form_elem as $elem) 
-					echo form_input_formatted($elem);?>
-			<?php echo form_close();?>
+			<?php echo generate_form('Admin/validate',$form_elem);?>
 			</div>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
