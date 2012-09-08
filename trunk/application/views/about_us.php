8545c6030720f53edcf4685a0aed45a713368c3c
aditya09007@iiitd.ac.in <aditya09007@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-09-08 10:10:04 +0000

    Intro Page updation

--- trunk/application/views/about_us.php
+++ trunk/application/views/about_us.php
@@ -15,9 +15,20 @@
 	<h1><center>Welcome to Hostel Allocation System - IIIT-D!<center></h1>
 		<div><h2>About Us</h2></div>
 		<div id="text">
-		This site created under the Software Enginnering Project. The project members are - praneet, kunal, mayank, sushant, manik, aditya, anirudh, karan, raghav
+		<p>This project is created with the intention to help institutes in the process of hostel allocation to students using distance as the primary measurement.
+		The team aims at building a highly scalable and generic software which will allow custom data entry and custom allotment parameters.
+		The project members are - praneet, kunal, mayank, sushant, manik, aditya, anirudh, karan, raghav</p>
 		</div>
-
+		<div id="text"><h3>About The System</h3>
+		<div>
+		<p>       When ready this software will facilitate: </p>
+		<ul>
+		<li>Registration form : Students can apply remotely through www.</li>
+		<li>Allocation : Software will try to allocate students based on distance</li>
+		<li>Administration : An authorised administrator can approve or disapprove the allotment.</li>
+		<li>Status : A status report visible on the front-end.</li>
+		<li>Anonymous Reporting : Anybody can report false data entered by some student anonymously .</li>
+	
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 	</div>
 </div>
