c29bc62ec6a96a1b3d894682ff88b406109d2a91
praneet10061 <praneet10061@iiitd.ac.in>
2012-09-08 15:07:08 +0000

    display_details.php - db info verification

--- application/views/display_details.php
+++ application/views/display_details.php
@@ -6,13 +6,22 @@
 	    <?php endforeach;?>
 </head>
 <body>
+
 <div class="container">
+
 	<div class="col1"> <?php echo $content_navigation?> </div>
-	<div class="col2">
-		Hi <?php echo $name; ?>.<br />
-		You are roll no. <?php echo $roll; ?>.<br />
-		your course is <?php echo $course; ?>.<br />
-		Your Prefer <?php echo $room_preference; ?>.<br />
+	<div class="col2" style="padding-left: 20px;padding-bottom: 10px">
+		<h1><center>Database Info Verification</center></h1>
+		Name: <b><?php echo $name; ?></b><br />
+		Roll No.: <b><?php echo $roll; ?></b><br />
+		Course: <b><?php echo $course; ?></b><br />
+		Room Preference: <b><?php echo $room_preference; ?></b><br />
+		Address: [will be taken from db] <br /><br />
+		
+		<button>Proceed</button>
+		&nbsp<button>Report</button>
+		
+		
 	</div>
 	</div>
 </body>
