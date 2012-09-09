3fb4f79de1a44aab4db047432257a4846e41439d
manik10044 <manik10044@iiitd.ac.in>
2012-09-09 16:54:29 +0000

    Email is now working

---
+++ application/config/email.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');  
+/* 
+| ------------------------------------------------------------------- 
+| EMAIL CONFING 
+| ------------------------------------------------------------------- 
+| Configuration of outgoing mail server. 
+| */   
+	$config['protocol'] = 'smtp';
+	$config['charset'] = 'iso-8859-1';
+	$config['smtp_host']='ssl://smtp.googlemail.com';  
+	$config['smtp_user'] = 'hosteliiitd@gmail.com';
+	$config['smtp_pass'] = 'hostel@123';
+	$config['smtp_port']=465;  
+	$config['wordwrap'] = TRUE;
+	$config['smtp_timeout']='30';
+	$config['mailtype']='html';
+	$config['crlf']="\r\n";
+	$config['newline']="\r\n";
+	
+	  
+	  
+/* End of file email.php */  
+/* Location: ./system/application/config/email.php */  
---
+++ application/controllers/email_user.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Email_user extends CI_Controller {
+	public function index(){
+		$this->load->library('email', $config);
+		$this->email->from('hosteliiitd@gmail.com'); // change it to yours
+		$this->email->to('cse300-group10@googlegroups.com'); // change it to yours
+		$this->email->subject('Test Mail');
+		$this->email->message('Code Igniter Mail Works.This is a system generated mail');
+ 
+		if($this->email->send())
+		{
+			echo 'Email sent.';
+			}
+			else
+			{
+				show_error($this->email->print_debugger());
+				}
+	}
+}
