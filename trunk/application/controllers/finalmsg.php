8b74755e6e5ebc12acd9e8fc63516621df2d50f8
praneet10061@iiitd.ac.in <praneet10061@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-09-09 16:12:14 +0000

    changes just for mock-up/prototype

--- trunk/application/controllers/finalmsg.php
+++ trunk/application/controllers/finalmsg.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class FinalM extends CI_Controller {
+class Final extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
