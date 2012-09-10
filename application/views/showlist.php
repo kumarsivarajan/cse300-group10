501f4bc7978f47f3e95884a2dd822ef9eb92a4d8
manik10044 <manik10044@iiitd.ac.in>
2012-09-10 15:17:31 +0000

    Added Templates and fixed go back

---
+++ application/views/showlist.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<?php foreach($css as $cssfile):?>
+	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile);?>"/>
+	<?php endforeach; ?>
+	<title>Hostel Allocation - Backend</title>
+</head>
+<body>
+
+<div class="admin-container">
+	<div class="admin-topbar"> <?php echo $top_menu?> </div>
+	<div class="admin-contentbar">
+	<!-- This where you write you code-->
+	Test123
+		
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	</div>
+</div>
+</body>
+</html>
