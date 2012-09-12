b9de76c75204cc730a834706be366e095b5485bf
mayank10048@iiitd.ac.in <mayank10048@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-09-12 20:10:50 +0000

    Created Change Password Form (Learnt how to do with controller, wud try to implement soon)

--- trunk/application/views/admin_manage.php
+++ trunk/application/views/admin_manage.php
@@ -12,8 +12,21 @@
 <div class="admin-container">
 	<div class="admin-topbar"> <?php echo $top_menu?> </div>
 	<div class="admin-contentbar">
-	<!-- This where you write you code-->
-	Test123
+	<!-- This where you write you code
+	<ol>
+	<li> abc</li>
+	<li>ddd</li> <br />
+	</ol>
+	
+	Testing things... <br /> <br /> -->
+	
+	<form action="just_testing_changepassword_thing.php" method="post">
+	Current Password: <input type="password" name="current_password"  /> <br /> <br />
+	New Password: <input type="password" name="new_password" /> <br /> <br />
+	Confirm New Password: <input type="password" name="confirm_new_password" /><br /><br />
+	<!-- <input type="button" value="CHANGE THE F****** PASSWORD!!" /> -->
+	<input type="submit" value="Change Password" />
+	</form>
 		
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 	</div>
