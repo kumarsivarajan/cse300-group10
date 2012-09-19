a11c4f27514edfab3ddc3a71b26fddedcf404e92
praneet10061@iiitd.ac.in <praneet10061@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-09-19 18:20:16 +0000

    basic false submission report page in ADMIN

---
+++ trunk/application/views/show_report_page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<?php foreach($css as $cssfile):?>
+	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile);?>"/>
+	<?php endforeach; ?>
+	<?php foreach($scripts as $script):?>
+	    <script type="text/javascript" src="<?php echo base_url("/application/js/".$script)?>"></script>
+	<?php endforeach; ?>
+	    <script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				$('#admin_list').dataTable();
+			} );
+		</script>
+	<title>False Submission Report</title>
+</head>
+<body>
+
+<div class="admin-container">
+	<div class="admin-topbar"> <?php echo $top_menu?> </div>
+	<div class="admin-contentbar" align=center>
+
+	<?php echo $table?>
+		
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	</div>
+</div>
+</body>
+</html>
