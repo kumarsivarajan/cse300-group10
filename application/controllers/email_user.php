da9281ad4a86ad17064584930258b969537cca2b
manik10044 <manik10044@iiitd.ac.in>
2012-09-20 03:45:14 +0000

    E-Mail Content

--- application/controllers/email_user.php
+++ application/controllers/email_user.php
@@ -10,11 +10,14 @@ class Email_user extends CI_Controller {
 		$emailTo=$student_data['email'];
 		$this->email->to($emailTo); // change it to yours
 		$this->email->subject('Hostel Application Form Verification');
-		$this->email->message('Verify your hostel application by clicking on this link\n');
+		$this->email->message('Verify your hostel application by clicking on this link:-
+		http://localhost/cse300-group10/index.php/address_maps');
  
 		if($this->email->send())
 		{
-			echo '<html> <body><h1>Email sent. Follow the link in the email to proceed</h1></body> </html>';
+			echo '<html> <body><h1>Email sent. Follow the link in the email to proceed</h1>
+			Mail Link : <a href="http://mail.iiitd.ac.in">IIITD:Webmail</a>
+			</body> </html>';
 		}
 		else
 		{
