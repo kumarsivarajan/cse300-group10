44b5298acd41c17e73386d0d2e60221564bcdb9e
praneet10061@iiitd.ac.in <praneet10061@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-10-20 11:14:16 +0000

    fixed small things - added admin home link to logo in backend, brought title to center

--- trunk/application/views/top_bar.php
+++ trunk/application/views/top_bar.php
@@ -1,5 +1,6 @@
-<h1><a href="#"> <img src="<?php echo base_url("/application/images/logo.png")?>" height="50" width="70"/></a>
+<h1><a href="<?php echo "http://localhost/cse300-group10/index.php/Admin_view" ?>"> <img src="<?php echo base_url("/application/images/logo.png")?>" height="50" width="70"/></a>
 <a href="<?php echo "http://www.iiitd.ac.in" ?>"> <img src="<?php echo base_url("/application/images/ins_logo.png")?>" height="50" /></a>
-<a class="title" >Hostel Allocation</a>
 <a class="logout_link" href="<?php echo site_url('Admin/logout');?>">Logout</a>
+<a class="title" ><center>Hostel Allocation</center></a>
+
 </h1>
