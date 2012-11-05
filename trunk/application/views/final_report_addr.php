eeb4defd19595cd08a8e86e6857e09b418845025
praneet10061@iiitd.ac.in <praneet10061@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-11-05 17:19:11 +0000

    wrong address reporting

---
+++ trunk/application/views/final_report_addr.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+
+<?php foreach($css as $cssfile):?>
+	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile)?>"/>
+	    <?php endforeach;?>
+</head>
+<body>
+
+<div class="container">
+
+	<div class="col1"> <?php echo $content_navigation?> </div>
+	<div class="col2">
+		<br /><br />
+		
+		
+		
+		&nbsp &nbsp Your report has been successfully submitted. The administrator will get in touch with you soon.
+		
+		
+		<!--<h1><center>Report Issue</center></h1>
+		<form method="post" action="">
+		<textarea name="Report issue" cols="50" rows="10">
+		Enter your comments here...
+		</textarea><br>
+		<input type="submit" value="Submit" />
+		</form>
+		</br> -->
+		
+
+		
+		
+		
+	</div>
+	</div>
+</body>
+</html>
+
