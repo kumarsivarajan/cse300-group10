2007d265553d58e5c2bffd450e243d191376a745
sushant10088@iiitd.ac.in <sushant10088@iiitd.ac.in@958ae824-1af9-fc4e-9711-44ccc4c0b2ff>
2012-11-08 12:27:11 +0000

    View for feedback_form created. Controller feedback_form() added. To be done:- Creating a link to this form on every page. Submit button on the page not working. I couldn't figure out where to direct it and how.

---
+++ trunk/application/views/feedback_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<?php foreach($css as $cssfile):?>
+	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile);?>"/>
+	<?php endforeach; ?>
+	<title>User feedback form</title>
+</head>
+<body>
+<div class="container">
+	<div class="col1"> <?php echo $content_navigation?> </div>
+	
+	<div class="col2" >
+	<h1><center>Feedback Form</center></h1>
+		<?php echo generate_form('Welcome/validate_student',$form_elem,$form_attr);?>
+	</div>
+	</div>
+</body>
