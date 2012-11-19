a3ea512a784613dc2a6cd68b9be6c380e387b27f
raghav10067 <raghav10067@iiitd.ac.in>
2012-11-19 11:18:06 +0000

    Icon added.

--- application/views/faq.php
+++ application/views/faq.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="icon" href="<?php echo base_url(); ?>/favicon2.png" type="image/png">
 <meta charset="utf-8"/>
 	<?php foreach($css as $cssfile):?>
 	    <link rel="stylesheet" type="text/css" href="<?php echo base_url("/application/css/".$cssfile);?>"/>
